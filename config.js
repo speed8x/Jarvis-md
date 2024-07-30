6ff991c1330be427c100a146c322a93d8265d2bf
speed8x <166624007+speed8x@users.noreply.github.com>
2024-07-30 07:04:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   WARN_COUNT: process.env.WARN_COUNT || '3',
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_1e64_b529_1b05_871f_381d_8eec_74f7_0b36',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_d163_98f9_9af3_03c0_d556_4b5d_34a5_17fd',
   LANG: process.env.LANG || 'EN',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLER  || '.',
